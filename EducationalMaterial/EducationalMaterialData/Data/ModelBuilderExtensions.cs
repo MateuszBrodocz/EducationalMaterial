c2047ef1518cb0915eab3119c5c9480cbf709d14
Mateusz Brodocz <Mateusz.Brodocz@motorolasolutions.com>
2021-04-28 20:29:34 +0200

    Add Seed For DbContext

---
+++ EducationalMaterial/EducationalMaterialData/Data/ModelBuilderExtensions.cs
@@ -0,0 +1,63 @@
+﻿using EducationalMaterialData.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EducationalMaterialData.Data
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>().HasData(
+                new Author
+                {
+                    AuthorId = 1,
+                    FirstName = "William",
+                    LastName = "Shakespeare"
+                }
+            );
+            modelBuilder.Entity<Material>().HasData(
+                new Material { 
+                    MaterialId = 2, 
+                    AuthorId = 1, 
+                    Description = ".NET Core 3.1 MVC REST API - Full Course",
+                    Url = "https://www.youtube.com/watch?v=fmvcAzHpsk8&t=7339s", 
+                    MaterialTypeId = 4 },
+                new Material { 
+                    MaterialId = 3, 
+                    AuthorId = 1, 
+                    Description = "Getting Started with EF Core",
+                    Url = "https://docs.microsoft.com/en-gb/ef/core/get-started/overview/first-app?tabs=netcore-cli", 
+                    MaterialTypeId = 5 }
+            );
+            modelBuilder.Entity<MaterialType>().HasData(
+                new MaterialType
+                {
+                    MaterialTypeId = 4,
+                    Name = "MVC REST API",
+                },
+                new MaterialType
+                {
+                    MaterialTypeId = 5,
+                    Name = "EF Core",
+                }
+            );
+            modelBuilder.Entity<Review>().HasData(
+                new Review
+                {
+                    ReviewId = 6,
+                    Description = "Good Material",
+                    MaterialId = 2
+                },
+                new Review
+                {
+                    ReviewId = 7,
+                    Description = "Nice One",
+                    MaterialId = 3
+                }
+            );
+        }
+    }
+}
---
+++ EducationalMaterial/EducationalMaterialData/Migrations/20210428075429_MigrationName.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EducationalMaterialData.Migrations
+{
+    public partial class MigrationName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MaterialTypes",
+                columns: table => new
+                {
+                    MaterialTypeId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MaterialTypes", x => x.MaterialTypeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Materials",
+                columns: table => new
+                {
+                    MaterialId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaterialTypeId = table.Column<int>(type: "int", nullable: false),
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Materials", x => x.MaterialId);
+                    table.ForeignKey(
+                        name: "FK_Materials_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "AuthorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Materials_MaterialTypes_MaterialTypeId",
+                        column: x => x.MaterialTypeId,
+                        principalTable: "MaterialTypes",
+                        principalColumn: "MaterialTypeId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaterialId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Materials_MaterialId",
+                        column: x => x.MaterialId,
+                        principalTable: "Materials",
+                        principalColumn: "MaterialId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materials_AuthorId",
+                table: "Materials",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Materials_MaterialTypeId",
+                table: "Materials",
+                column: "MaterialTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_MaterialId",
+                table: "Reviews",
+                column: "MaterialId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Materials");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "MaterialTypes");
+        }
+    }
+}
